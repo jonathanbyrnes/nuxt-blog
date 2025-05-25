cac42d2247b8daa222db0428888b39533be8a5e5
Jonathan Byrnes <48105542+jonathanbyrnes@users.noreply.github.com>
2025-05-25 20:13:34 +0100

    feat: create initial default layout

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div>
+            <ul>
+                <li>
+                    <NuxtLink to="">Home</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="">Articles</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="">Register</NuxtLink>
+                </li>
+            </ul>
+        </div>
+        <br>
+        <br>
+        <br>
+        
+        <div>
+            <slot></slot>
+        </div>
+
+        <div>
+            <h1>This is the footer</h1>
+        </div>
+
+    </div>
+</template>
