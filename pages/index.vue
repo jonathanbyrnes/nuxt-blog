e3a5c1decd1e3c1e3fc14336584eef426863fbee
Jonathan Byrnes <48105542+jonathanbyrnes@users.noreply.github.com>
2025-05-25 20:23:27 +0100

    feat: add article listing to initial page

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,14 @@
 definePageMeta({
     layout: 'default'
 })
+const articles = ref([1, 2, 3, 4, 5])
 </script>
 
 <template>
     <div>
-        <h1>Welcome Page</h1>
+        <div v-for="(index, article) in articles" :key="index">
+            <h1>Article - {{ article }}</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum illum asperiores, ad illo tenetur officiis aliquid distinctio. Provident cumque magni fuga maiores adipisci ducimus dolor magnam cupiditate, accusantium quasi eaque.</p>
+        </div>
     </div>
 </template>
