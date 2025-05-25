c9497ee29d32988afd5c21feaf09c770df518dcf
BluntPikachu <48105542+BluntPikachu@users.noreply.github.com>
2025-05-25 20:26:24 +0100

    fix: adjust spacing for article display on initial page

--- pages/index.vue
+++ pages/index.vue
@@ -7,9 +7,15 @@ const articles = ref([1, 2, 3, 4, 5])
 
 <template>
     <div>
+        <div>
+            <h1>List of Articles</h1>
+        </div>
+
         <div v-for="(index, article) in articles" :key="index">
-            <h1>Article - {{ article }}</h1>
+            <h2>Article - {{ article }}</h2>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum illum asperiores, ad illo tenetur officiis aliquid distinctio. Provident cumque magni fuga maiores adipisci ducimus dolor magnam cupiditate, accusantium quasi eaque.</p>
         </div>
+        <br>
+        
     </div>
 </template>
