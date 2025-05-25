950b6d51e044e5bf3c8435d572cd718c227874c3
Jonathan Byrnes <48105542+jonathanbyrnes@users.noreply.github.com>
2025-05-25 20:49:25 +0100

    feat: add admin blank pages (articles, create post, and dashboard)

---
+++ layouts/admin.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+
+        
+        <div>
+            <slot></slot>
+        </div>
+
+        <div style="background-color: gainsboro;">
+            <h1>Admin Footer</h1>
+        </div>
+
+    </div>
+</template>
---
+++ pages/admin/articles.vue
@@ -0,0 +1,11 @@
+<script setup>
+definePageMeta({
+    layout: 'admin'
+})
+</script>
+
+<template>
+    <div>
+        <h1>Articles</h1>
+    </div>
+</template>
