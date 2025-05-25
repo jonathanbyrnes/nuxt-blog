47d553613489f648b62ebf4f772844d436121e68
Jonathan Byrnes <48105542+jonathanbyrnes@users.noreply.github.com>
2025-05-25 20:45:37 +0100

    feat: add auth layout to login/register

---
+++ layouts/auth.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+
+        
+        <div>
+            <slot></slot>
+        </div>
+
+        <div style="background-color: gainsboro;">
+            <h1>Auth Footer</h1>
+        </div>
+
+    </div>
+</template>
