7bf4fbaf3d4381fba8ba11634a4c1ff4d1af7779
BluntPikachu <48105542+BluntPikachu@users.noreply.github.com>
2025-05-26 22:02:50 +0100

    feat: create link component for admin layout

---
+++ components/admin/Links.vue
@@ -0,0 +1,34 @@
+<script setup>
+const links = [
+    {
+        name: "Dashboard",
+        path: "/admin/dashboard",
+    },
+    {
+        name: "Articles",
+        path: "/admin/articles",
+    },
+];
+</script>
+
+<template>
+    <ul class="flex gap-2 py-2 px-2">
+        <li v-for="link in links" :key="link.path" class="text-sm hover:bg-indigo-700 font-semibold hover:text-white py-2 px-2 rounded-md">
+            <NuxtLink :to="link.path">{{ link.name }}</NuxtLink>
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+    --tw-bg-opacity: 1;
+    color: white;
+    background-color: rgb(67 56 202 / var(--tw-bg-opacity, 1));
+    font-weight: 600;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    padding-left: 0.5rem;
+    padding-right: 0.5rem;
+    border-radius: 0.375rem;
+}
+</style>
