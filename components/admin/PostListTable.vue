7ca3f3fd4510865cb19487532bf10439b79049e8
BluntPikachu <48105542+BluntPikachu@users.noreply.github.com>
2025-05-27 14:01:25 +0100

    feat: create article table component + begin use in articles page

---
+++ components/admin/PostListTable.vue
@@ -0,0 +1,54 @@
+<script setup>
+defineProps(['posts', 'status'])
+
+const emit = defineEmits(['searchPost'])
+const query = ref('')
+
+async function searchPostByTitle() {
+    await emit('searchPost', query.value)
+}
+</script>
+
+<template>
+    <div>
+        <div class="flex justify-between mb-2">
+            <input @keydown="searchPostByTitle" v-model="query" placeholder="Search..." type="text"
+                class="mb-2 border rounded-md py-1 px-2 shadow-md">
+            <NuxtLink to="/admin/create-post"
+                class="rounded-md text-white px-2 py-2 bg-indigo-700 text-sm font-semibold">
+                Create a Post
+            </NuxtLink>
+        </div>
+
+        <table class="bg-white rounded-md w-full shadow-md border border-gray-300">
+            <thead>
+                <tr class="bg-gray-100 text-left">
+                    <td class="border border-gray-300 py-2 px-4">Image</td>
+                    <td class="border border-gray-300 py-2 px-4">Article-Title</td>
+                    <td class="border border-gray-300 py-2 px-4">Upload</td>
+                    <td class="border border-gray-300 py-2 px-4">Edit</td>
+                    <td class="border border-gray-300 py-2 px-4">Delete</td>
+
+                </tr>
+            </thead>
+
+            <tbody>
+                <div v-if="status === 'pending'">
+                    Loading data...
+                </div>
+                <tr v-else class="text-left" v-for="post in posts" :key="post?.id">
+                    <td class="border border-gray-300 py-2 px-4">
+                        <img :src="post?.image" width="150" />
+                    </td>
+                    <td class="border border-gray-300 py-2 px-4"> {{ post?.title }}</td>
+                    <td class="border border-gray-300 py-2 px-4"></td>
+
+                    <td class="border border-gray-300 py-2 px-4"></td>
+
+                    <td class="border border-gray-300 py-2 px-4"></td>
+
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
--- pages/admin/articles.vue
+++ pages/admin/articles.vue
@@ -2,10 +2,37 @@
 definePageMeta({
     layout: 'admin'
 })
+const config=useRuntimeConfig()
+const userData=getUserData()
+const query=ref('')
+const { data,error,status,refresh } = await useFetch(config.public?.API_BASE_URL+ "/posts", {
+    headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${userData?.token}`
+    },
+    watch: [query],
+    query: {
+        query:query.value
+    }
+})
+
+function _debounce(cb,delay) {
+    let timer
+    return function(...args) {
+        clearTimeout(timer)
+        timer=setTimeout(()=>cb(args),delay)
+    }
+}
+
+const searchPost=_debounce(function(searchVal){
+    console.log(searchVal)
+})
+
 </script>
 
 <template>
     <div>
-        <h1>Articles</h1>
+        <h1 class="text-2xl mb-2">Articles</h1>
+        <PostListTable @searchPost="searchPost" :status="status" :post="data?.data"/>
     </div>
 </template>
