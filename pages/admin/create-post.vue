2653a09232702bf42803c1bcce50cf365bff2857
BluntPikachu <48105542+BluntPikachu@users.noreply.github.com>
2025-05-27 14:01:42 +0100

    feat: add logic for creating an article(post)

--- pages/admin/create-post.vue
+++ pages/admin/create-post.vue
@@ -2,10 +2,59 @@
 definePageMeta({
     layout: 'admin'
 })
+
+const userData=getUserData()
+const config = useRuntimeConfig()
+const loading=ref(false)
+const postInput=ref({
+    title:'',
+    post_content:''
+})
+
+async function createPost() {
+    try {
+        loading.value=true
+        const res = await $fetch(config.public?.API_BASE_URL + '/posts', {
+            headers: {
+                Accept: "application/json",
+                "content-type": "application/json",
+                Authorization: `Bearer ${userData?.token}`
+            },
+            method: 'POST',
+            body: JSON.stringify(postInput.value)
+        })
+        loading.value=false
+        postInput.value={}
+        successMsg(res?.message)
+    } catch (error) {
+        loading.value=false
+        if(error?.response?.status===422){
+            const errors=error.response?._data
+            for(const message of errors) {
+                errorMsg(message)
+            }
+        }
+    }
+}
 </script>
 
 <template>
     <div>
-        <h1>Create Post</h1>
+        <h1 class="text-2xl mb-2">Create Post</h1>
+        <div class="flex flex-col mb-2 w-[600px]">
+            <input v-model="postInput.title" placeholder="Title..." type="text" class="mb-2 border rounded-md py-1 px-2 shadow-md" />
+            <textarea v-model="postInput.post_content" name="" id="" rows="5" class="mb-2 border rounded-md py-1 px-2 shadow-md"></textarea>
+        </div>
+
+        <div class="flex justify-between">
+            <NuxtLink to="/admin/articles" class="rounded-me text-gray-700 hover:text-white">
+                Back
+            </NuxtLink>
+            <button :disabled="loading" @click="createPost" class="rounded-md text-white px-2 py-2 bg-indigo-700 text-sm font-semibold">
+                {{ loading?'Processing...':'Create Post'}}
+            </button>
+
+
+        </div>
     </div>
 </template>
