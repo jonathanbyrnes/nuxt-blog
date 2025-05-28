47e2c990f81a6b8fba440ec21973ab89157faf4d
BluntPikachu <48105542+BluntPikachu@users.noreply.github.com>
2025-05-28 23:39:40 +0100

    feat: add api request to deletepost

--- pages/admin/articles.vue
+++ pages/admin/articles.vue
@@ -3,63 +3,79 @@ definePageMeta({
     layout: 'admin'
 })
 const mockPaginationData = {
-  data: [
-    { id: 1, title: 'Post 1' },
-    { id: 2, title: 'Post 2' },
-    { id: 3, title: 'Post 3' },
-  ],
-  meta: {
-    current_page: 1,
-    last_page: 1,
-    from: 1,
-    to: 3,
-    per_page: 10,
-    total: 3
-  },
-  links: {
-    first: '/?page=1',
-    last: '/?page=1',
-    prev: null,
-    next: null
-  }
+    data: [
+        { id: 1, title: 'Post 1' },
+        { id: 2, title: 'Post 2' },
+        { id: 3, title: 'Post 3' },
+    ],
+    meta: {
+        current_page: 1,
+        last_page: 1,
+        from: 1,
+        to: 3,
+        per_page: 10,
+        total: 3
+    },
+    links: {
+        first: '/?page=1',
+        last: '/?page=1',
+        prev: null,
+        next: null
+    }
 };
 
-const config=useRuntimeConfig()
-const query=ref('')
-const userData=getUserData()
-const page=ref(1)
-const { data,error,status,refresh } = await useFetch(config.public?.API_BASE_URL+ "/posts", {
+const config = useRuntimeConfig()
+const query = ref('')
+const userData = getUserData()
+const page = ref(1)
+const { data, error, status, refresh } = await useFetch(config.public?.API_BASE_URL + "/posts", {
     headers: {
         Accept: 'application/json',
         Authorization: `Bearer ${userData?.token}`
     },
-    watch: [query,page],
+    watch: [query, page],
     query: {
-        query:query.value,
-        page:page
+        query: query.value,
+        page: page
     }
 })
 
-function _debounce(cb,delay) {
+function _debounce(cb, delay) {
     let timer
-    return function(...args) {
+    return function (...args) {
         clearTimeout(timer)
-        timer=setTimeout(()=>cb(args),delay)
+        timer = setTimeout(() => cb(args), delay)
     }
 }
 
-const searchPost=_debounce(function(searchVal){
-    query.value=searchVal[0]
-},200)
+const searchPost = _debounce(function (searchVal) {
+    query.value = searchVal[0]
+}, 200)
 
-const paginateData=async(newPageVal) => {
-    page.value=newPageVal
+const paginateData = async (newPageVal) => {
+    page.value = newPageVal
     await refresh
 }
 
 function deletePost(id) {
-    promptUser('Are you sure you want to delete this?').then(function() {
-        console.log('...')
+    promptUser('Are you sure you want to delete this?').then(async function () {
+        try {
+            loading.value = true
+            const res = await $fetch(config.public?.API_BASE_URL + '/posts/' + id, {
+                headers: {
+                    Accept: "application/json",
+                    "content-type": "application/json",
+                    Authorization: `Bearer ${userData?.token}`
+                },
+                method: 'DELETE',
+            })
+            loading.value = false
+            await refresh
+            successMsg(res?.message)
+        } catch (error) {
+            loading.value = false
+            errorMsg(error?.message)
+        }
     })
 }
 
@@ -68,21 +84,17 @@ function deletePost(id) {
 <template>
     <div>
         <h1 class="text-2xl mb-2">Articles</h1>
-        <PostListTable @searchPost="searchPost" :status="status" :post="data?.data?.data"/>
+        <PostListTable @searchPost="searchPost" :status="status" :post="data?.data?.data" />
 
-        <TailwindPagination
-        class="mt-2"
-        :data="mockPaginationData"
-        @pagination-change-page="paginateData"
-        @delete-post="deletePost"
-        />
+        <TailwindPagination class="mt-2" :data="mockPaginationData" @pagination-change-page="paginateData"
+            @delete-post="deletePost" />
 
     </div>
 </template>
 <style scoped>
 button.relative.inline-flex.items-center.px-4.py-2.text-sm.font-medium.border.focus\:z-20.bg-blue-50.border-blue-500.text-blue-600.z-30 {
-  background: #4f46e5;
-  color: white;
-  border-radius: 5px;
+    background: #4f46e5;
+    color: white;
+    border-radius: 5px;
 }
 </style>
