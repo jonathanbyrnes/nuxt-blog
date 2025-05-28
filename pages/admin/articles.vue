a7721053c49458cd4f9f36fbbec4430a9f80dbae
Jonathan Byrnes <48105542+jonathanbyrnes@users.noreply.github.com>
2025-05-28 22:59:07 +0100

    feat: add pagination to articles

--- pages/admin/articles.vue
+++ pages/admin/articles.vue
@@ -5,14 +5,16 @@ definePageMeta({
 const config=useRuntimeConfig()
 const userData=getUserData()
 const query=ref('')
+const page=ref(1)
 const { data,error,status,refresh } = await useFetch(config.public?.API_BASE_URL+ "/posts", {
     headers: {
         Accept: 'application/json',
         Authorization: `Bearer ${userData?.token}`
     },
-    watch: [query],
+    watch: [query,page],
     query: {
-        query:query.value
+        query:query.value,
+        page:page
     }
 })
 
@@ -25,14 +27,24 @@ function _debounce(cb,delay) {
 }
 
 const searchPost=_debounce(function(searchVal){
-    console.log(searchVal)
-})
+    query.value=searchVal[0]
+},200)
+
+const paginateData=async(newPageVal) => {
+    page.value=newPageVal
+    await refresh
+}
 
 </script>
 
 <template>
     <div>
         <h1 class="text-2xl mb-2">Articles</h1>
-        <PostListTable @searchPost="searchPost" :status="status" :post="data?.data"/>
+        <PostListTable @searchPost="searchPost" :status="status" :post="data?.data?.data"/>
+
+        <TailwindPagination
+        :data="data?.data"
+        @pagination-change-page="paginateData"
+        />
     </div>
 </template>
