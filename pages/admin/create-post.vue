7eadb4e5788196ab587068baf798f9ef1651d265
Jonathan Byrnes <48105542+jonathanbyrnes@users.noreply.github.com>
2025-05-29 01:45:58 +0100

    feat: add rich-editor for article creations

--- pages/admin/create-post.vue
+++ pages/admin/create-post.vue
@@ -13,16 +13,19 @@ const { postInput, loading, edit } = storeToRefs(postStore)
         <div class="flex flex-col mb-2 w-[600px]">
             <input v-model="postInput.title" placeholder="Title..." type="text"
                 class="mb-2 border rounded-md py-1 px-2 shadow-md" />
-            <textarea v-model="postInput.post_content" name="" id="" rows="5"
-                class="mb-2 border rounded-md py-1 px-2 shadow-md"></textarea>
+
+            <ClientOnly fallback-tag="span" fallback="Loading comments">
+                <rich-editor :value="postInput.post_content" @input="event => postInput.post_content = event" />
+            </ClientOnly>
+
         </div>
 
         <div class="flex justify-between">
-            <NuxtLink to="/admin/articles" class="rounded-me text-gray-700 hover:text-white">
+            <NuxtLink to="/admin/articles" class="rounded-md text-gray-700 hover:text-white hover:bg-indigo-700 px-2 py-2 text-sm font-semibold">
                 Back
             </NuxtLink>
             <button :disabled="loading" @click="postStore.createPost"
-                :class="edit ? 'bg-yellow-500 rounded-md text-white px-2 py-2 text-sm font-semibold' : 'bg-indigo-700rounded-md text-white px-2 py-2 text-sm font-semibold'">
+                :class="edit ? 'bg-yellow-500 rounded-md text-white px-2 py-2 text-sm font-semibold' : 'bg-indigo-700 rounded-md text-white px-2 py-2 text-sm font-semibold'">
                 {{ loading ? 'Processing...' : edit ? 'Update Post' : 'Create Post' }}
             </button>
 
