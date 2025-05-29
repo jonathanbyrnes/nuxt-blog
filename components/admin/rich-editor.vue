96b32c8cb13a197ec369848587b27962dea6c621
BluntPikachu <48105542+BluntPikachu@users.noreply.github.com>
2025-05-29 01:45:58 +0100

    feat: add rich-editor for article creations

---
+++ components/admin/rich-editor.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps(['value'])
+const emit = defineEmits(['input'])
+
+const emitInput = (event) => {
+  emit("input", event)
+}
+const { $ckeditor } = useNuxtApp()
+const editor = $ckeditor.classicEditor;
+
+</script>
+
+<template>
+  <ckeditor :editor="editor" :modelValue="value" @update:modelValue="emitInput" />
+
+</template>
